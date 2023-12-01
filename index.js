1a64868ca3dc1f320b2265894ba1b5e416e1f666
ctasbihas <ctasbihas@gmail.com>
2023-12-01 20:18:08 +0600

    bd

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send("Hello Bangladesh!");
 });
 
 const client = new MongoClient(process.env.DB_URI, {
